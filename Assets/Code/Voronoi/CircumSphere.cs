aa8faf8522550d5854b31594a257eb0095b46cb2
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2023-02-11 11:29:23 +0000

    Add Bounds to CircumSphere

--- Assets/Code/Voronoi/CircumSphere.cs
+++ Assets/Code/Voronoi/CircumSphere.cs
@@ -13,11 +13,14 @@ namespace Growth.Voronoi
             Valid = css.Valid;
             Centre = css.Centre;
             Radius = (float)css.Radius;
+            var half_widths = new Vec3(Radius, Radius, Radius);
+            Bounds = new VBounds(Centre - half_widths, Centre + half_widths);
         }
 
         public bool Valid { get; }
         public Vec3 Centre { get; }
         public float Radius { get; }
+        public VBounds Bounds { get; internal set; }
 
         // we must overlap by more than t to be considered containing,
         // set t -ve if you want to make the test looser
